f23c007fc1a0d8be749e9e92635e6c7677c34a46
Xing Chen <xchen601@gmail.com>
2024-11-03 00:42:48 -0400

    initial commit

---
+++ src/main/java/com/example/localloop/LocalLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.localloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalLoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/localloop/LocalLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
